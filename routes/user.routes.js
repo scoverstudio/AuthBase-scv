744afb507aff2dc506fd11921950f91c0b4ffa39
“Krystian” <scoverstudio@gmail.com>
2022-04-11 14:01:25 +0200

    add google login

---
+++ routes/user.routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/logged", (req, res) => {
+  if (req.user) {
+    res.render("logged", {
+      name: req.user.displayName,
+      photo: req.user.photos[0].value,
+    });
+  } else {
+    res.redirect("/no-permission");
+  }
+});
+
+router.get("/no-permission", (req, res) => {
+  res.render("noPermission");
+});
+
+router.get("/profile", (req, res) => {
+  if (req.user) {
+    res.render("profile");
+  } else {
+    res.redirect("/no-permission");
+  }
+});
+router.get("/profile/settings", (req, res) => {
+  if (req.user) {
+    res.render("profileSettings");
+  } else {
+    res.redirect("/no-permission");
+  }
+});
+
+module.exports = router;
